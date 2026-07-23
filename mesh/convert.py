a8676ec28ea24c1104dac31b4d25761d60b96e5d
PresentFox <143633123+PresentFoxz@users.noreply.github.com>
2026-07-23 01:29:26 -0400

    Update convert.py

--- mesh/convert.py
+++ mesh/convert.py
@@ -2,9 +2,7 @@ import sys
 import os
 import random
 
-
 def convert_obj(input_file, output_file):
-
     vertices = []
     triangles = []
 
@@ -14,74 +12,37 @@ def convert_obj(input_file, output_file):
 
             if line.startswith("v "):
                 _, x, y, z = line.split()[:4]
-
-                vertices.append((
-                    float(x),
-                    float(y),
-                    float(z)
-                ))
-
-
+                vertices.append((float(x), float(y), float(z)))
+            
             elif line.startswith("f "):
                 parts = line.split()[1:]
-
                 indices = []
 
                 for p in parts[:3]:
                     index = p.split("/")[0]
-
-                    # OBJ is 1-based
-                    # FOX is 0-based
                     indices.append(int(index) - 1)
 
-
                 color = random.randint(0, 255)
-
-                triangles.append((
-                    indices[0],
-                    indices[1],
-                    indices[2],
-                    color,
-                    0
-                ))
+                triangles.append((indices[0], indices[1], indices[2], color, 0))
 
 
     with open(output_file, "w") as out:
-
-        # vertices
         for x, y, z in vertices:
-            out.write(
-                f"v {x} {y} {z}\n"
-            )
-
-
+            out.write(f"v {x} {y} {z}\n")
+        
         out.write("\n")
 
-
-        # triangles
         for a, b, c, color, bfc in triangles:
-            out.write(
-                f"f {a} {b} {c} {color} {bfc}\n"
-            )
-
+            out.write(f"f {a} {b} {c} {color} {bfc}\n")
 
 if __name__ == "__main__":
-
     if len(sys.argv) < 3:
-        print(
-            "Usage: python obj_to_fox.py input.obj output.fox"
-        )
+        print("Usage: python obj_to_fox.py input.obj output.fox")
         sys.exit(1)
-
 
     obj = sys.argv[1]
     out = sys.argv[2]
 
     convert_obj(obj, out)
 
-    print(
-        "Converted:",
-        obj,
-        "->",
-        out
-    )
+    print("Converted:", obj, "->", out)
